761c6d28f212116cfc9fa349d3ea6c87ac44ea70
김동현 <47862506+PH5555@users.noreply.github.com>
2025-08-01 21:57:54 +0900

    feat: 이메일 전송 메시지 rabbitmq로 전송

--- src/main/java/com/zkrypto/zkMatch/global/rabbitmq/DirectExchangeService.java
+++ src/main/java/com/zkrypto/zkMatch/global/rabbitmq/DirectExchangeService.java
@@ -1,17 +1,26 @@
 package com.zkrypto.zkMatch.global.rabbitmq;
 
+import com.zkrypto.zkMatch.domain.corporation.domain.entity.Corporation;
+import com.zkrypto.zkMatch.domain.member.domain.entity.Member;
+import com.zkrypto.zkMatch.global.rabbitmq.dto.SendMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class DirectExchangeService {
     private final RabbitTemplate rabbitTemplate;
-    private final String DIRECT_EXCHANGE_NAME = "email_exchange";
-    private final String DIRECT_QUEUE_ROUTING_KEY = "direct_routing_key";
 
-    public void send() {
+    @Value("${spring.rabbitmq.template.exchange}")
+    private String exchange;
 
+    @Value("${spring.rabbitmq.template.routing-key}")
+    private String routingKey;
+
+    public void send(Corporation corporation, Member member) {
+        SendMessage message = SendMessage.from(corporation, member);
+        rabbitTemplate.convertAndSend(exchange, routingKey, message);
     }
 }
