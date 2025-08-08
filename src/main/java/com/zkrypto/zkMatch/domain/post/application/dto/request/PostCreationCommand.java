91e416eb5a21ace5cc7941ceeba10861d247dc09
김동현 <47862506+PH5555@users.noreply.github.com>
2025-08-08 16:19:25 +0900

    add: 공고 생성 requestDto 수정

--- src/main/java/com/zkrypto/zkMatch/domain/post/application/dto/request/PostCreationCommand.java
+++ src/main/java/com/zkrypto/zkMatch/domain/post/application/dto/request/PostCreationCommand.java
@@ -8,13 +8,16 @@ import java.util.List;
 @Getter
 public class PostCreationCommand {
     private String title;
+    private String content;
     private LocalDateTime startDate;
     private LocalDateTime endDate;
-    private String schoolHistory;
-    private String jobHistory;
-    private List<String> licenses;
-    private int salary;
-    private String location;
-    private String jobType;
-    private int recruitCount;
+    private String majorRequirement;
+    private String educationRequirement;
+    private String experienceRequirement;
+    private List<String> licenseRequirement;
+    private int salaryStart;
+    private int salaryEnd;
+    private String workSpace;
+    private String workType;
+    private List<String> preferredSkill;
 }
--- src/main/java/com/zkrypto/zkMatch/domain/post/domain/entity/Post.java
+++ src/main/java/com/zkrypto/zkMatch/domain/post/domain/entity/Post.java
@@ -2,11 +2,13 @@ package com.zkrypto.zkMatch.domain.post.domain.entity;
 
 import com.zkrypto.zkMatch.domain.corporation.domain.entity.Corporation;
 import com.zkrypto.zkMatch.domain.post.application.dto.request.PostCreationCommand;
+import com.zkrypto.zkMatch.global.utils.StringListConverter;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -17,22 +19,60 @@ public class Post {
     private UUID postId;
 
     private String title;
-
+    private String content;
     private LocalDateTime startDate;
     private LocalDateTime endDate;
+    private String majorRequirement;
+    private String educationRequirement;
+    private String experienceRequirement;
+
+    @Convert(converter = StringListConverter.class)
+    private List<String> licenseRequirement;
+    private int salaryStart;
+    private int salaryEnd;
+    private String workSpace;
+    private String workType;
+
+    @Convert(converter = StringListConverter.class)
+    private List<String> preferredSkill;
 
     @ManyToOne
     @JoinColumn(name = "corporation_id")
     private Corporation corporation;
 
-    public Post(String title, Corporation corporation, LocalDateTime startDate, LocalDateTime endDate) {
+    public Post(String title, String content, LocalDateTime startDate, LocalDateTime endDate, String majorRequirement, String educationRequirement, String experienceRequirement, List<String> licenseRequirement, int salaryStart, int salaryEnd, String workSpace, String workType, List<String> preferredSkill, Corporation corporation) {
         this.title = title;
-        this.corporation = corporation;
+        this.content = content;
         this.startDate = startDate;
         this.endDate = endDate;
+        this.majorRequirement = majorRequirement;
+        this.educationRequirement = educationRequirement;
+        this.experienceRequirement = experienceRequirement;
+        this.licenseRequirement = licenseRequirement;
+        this.salaryStart = salaryStart;
+        this.salaryEnd = salaryEnd;
+        this.workSpace = workSpace;
+        this.workType = workType;
+        this.preferredSkill = preferredSkill;
+        this.corporation = corporation;
     }
 
     public static Post from(PostCreationCommand command, Corporation corporation) {
-        return new Post(command.getTitle(), corporation, command.getStartDate(), command.getEndDate());
+        return new Post(
+                command.getTitle(),
+                command.getContent(),
+                command.getStartDate(),
+                command.getEndDate(),
+                command.getMajorRequirement(),
+                command.getEducationRequirement(),
+                command.getExperienceRequirement(),
+                command.getLicenseRequirement(),
+                command.getSalaryStart(),
+                command.getSalaryEnd(),
+                command.getWorkSpace(),
+                command.getWorkType(),
+                command.getPreferredSkill(),
+                corporation
+        );
     }
 }
